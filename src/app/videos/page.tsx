386bf0178f3a773ce2ea72085dc1ce7b7f74018e
Alexandre-200 <alexandre_200@outlook.com.br>
2024-04-14 16:29:21 -0300

    teste update vercel

--- src/app/videos/page.tsx
+++ src/app/videos/page.tsx
@@ -4,7 +4,7 @@ export default function Videos() {
   return (
     <div className="flex justify-center items-center ">
       <div className="gap-5  xl:grid xl:grid-cols-2 " >
-        <p className="">Os vídeo serão adicionados em breve.</p>
+        <p className="">Os vídeos serão adicionados em breve.</p>
         {
           // <Video videoId="rTj4z2Pur10"  width={460} height={260} titulo="teste1"/>
           //<Video videoId="rTj4z2Pur10"  width={460} height={260} titulo="teste2"/>
