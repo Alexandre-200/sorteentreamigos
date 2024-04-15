dca8cdf2b7706b1baa8aa57baca0fb353406c277
Alexandre-200 <alexandre_200@outlook.com.br>
2024-04-15 09:34:14 -0300

    tratando erros footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
     return (
         <div className="flex flex-col justify-center items-center gap-4">
             <p className=" flex flex-auto text-justify max-w-xs uppercase mt-10">Que a sorte nos sorria, que os números se alinhem e que cada sorteio seja uma chance a mais para realizarmos nossos sonhos</p>
-            <Image className="rounded-full orange-shadow mt-10"
+            <Image className="rounded-full orange-shadow mt-10 mb-10"
                 src="/trevo.png"
                 width={150}
                 height={150}
